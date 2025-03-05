c09e3015a7a60177525ac36425d3376adf4bfb2b
Abid <abid.ar2k@gmail.com>
2025-03-05 23:32:52 +0530

    second commit - extract TEXT from PDF

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from  PyPDF2 import PdfReader
 
 def main():
     load_dotenv()
     st.set_page_config(page_title = "My PDF Reader")
     st.header("My PDF Reader")
     
+    
+    #uplaod the texr
     pdf = st.file_uploader("Upload your PDF file", type = "pdf")
+    
+    #extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        st.write(text)
 
 
 
