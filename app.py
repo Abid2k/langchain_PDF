7d6604396a75d16270bc002fb63ff3add1925fbc
Abid <abid.ar2k@gmail.com>
2025-03-08 00:55:18 +0530

    LAST COMMIT

--- app.py
+++ app.py
@@ -4,13 +4,25 @@ import streamlit as st
 from  PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+# from langchain_openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 
 def main():
     load_dotenv()
+    
+    google_api_key = os.getenv("GEMENI_API_KEY")
+    if not google_api_key:
+      st.error("API KEY NOT FOUND")
+      return
+
+    
     st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
+    st.header("Ask your PDF ")
     
     # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
@@ -32,11 +44,23 @@ def main():
       chunks = text_splitter.split_text(text)
       
       # create embeddings
-      embeddings = OpenAIEmbeddings()
+      # embeddings = OpenAIEmbeddings()
+      embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",api_key=google_api_key)
       knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    user_question = st.text_input("Ask question about your pdf here")
-
+      user_question = st.text_input("Ask question about your pdf here")
+      
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        # st.write(docs)
+        
+        # llm = OpenAi()
+        llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
+        chain = load_qa_chain(llm, chain_type="stuff")
+        responce = chain.run(input_documents=docs, questions=user_question)
+        
+        st.write(responce)
+      
 if __name__ == '__main__':
      main()
 
